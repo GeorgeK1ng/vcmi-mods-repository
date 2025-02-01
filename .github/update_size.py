5c4d65b9f293849c708b45acdf31c5088b80cd35
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2025-02-01 21:54:24 +0100

    Update update_size.py

--- .github/update_size.py
+++ .github/update_size.py
@@ -3,7 +3,6 @@ import glob
 import os
 import sys
 import urllib.request
-from io import StringIO
 
 from ignore_json import ignore
 
@@ -15,7 +14,6 @@ for filename in glob.glob(os.path.join('.', '*.json')):
             filecontent = file.read()
 
         try:
-            jstyleson.load(StringIO(filecontent))
             modlist = jstyleson.loads(filecontent)
         except Exception as err:
             print(f"❌ Error reading JSON file {filename}: {err}")
@@ -27,21 +25,14 @@ for filename in glob.glob(os.path.join('.', '*.json')):
 
             try:
                 response = urllib.request.urlopen(url)
+                filecontent = response.read()
                 print(f"✅ Download successful")
             except Exception as err:
                 print(f"❌ Download failed: {err}")
                 sys.exit(os.EX_SOFTWARE)
 
-            try:
-                filecontent = response.read().decode("utf-8")
-                jstyleson.load(StringIO(filecontent))
-                print(f"✅ JSON valid")
-            except Exception as err:
-                print(f"❌ JSON invalid: {err}")
-                sys.exit(os.EX_SOFTWARE)
-
-            filesize = round(len(filecontent.encode('utf-8')) / 1024 / 1024, 3)
-            print(f"Size: {filesize}")
+            filesize = round(len(filecontent) / 1024 / 1024, 3)
+            print(f"Size: {filesize} MB")
             data["downloadSize"] = filesize
 
         resultcontent = json.dumps(modlist, indent='\t', separators=(',', ' : ')) + "\n"

6b01bf44baf4c57b43d7fa6d9af73047a89044a2
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2025-02-01 23:15:22 +0100

    Update update_size.py

--- .github/update_size.py
+++ .github/update_size.py
@@ -1,4 +1,3 @@
-import jstyleson
 import json
 import glob
 import os
@@ -10,36 +9,23 @@ from ignore_json import ignore
 for filename in glob.glob(os.path.join('.', '*.json')):
     if filename not in ignore:
         print(f"Opening: {filename}")
-
-        with open(filename, "r") as file:
-            filecontent = file.read()
-
-        try:
-            modlist = jstyleson.loads(filecontent)
-        except Exception as err:
-            print(f"❌ Error reading JSON file {filename}: {err}")
-            sys.exit(os.EX_SOFTWARE)
-
+        filecontent = open(filename, "r").read()
+        modlist = json.loads(filecontent)
         for mod, data in modlist.items():
             url = data["download"].replace(" ", "%20")
             print(f"Download {mod}: {url}")
-
             try:
                 response = urllib.request.urlopen(url)
-                filecontent = response.read()
-                print(f"✅ Download successful")
-            except Exception as err:
-                print(f"❌ Download failed: {err}")
+            except:
+                print("Error: download failed!")
                 sys.exit(os.EX_SOFTWARE)
-
-            filesize = round(len(filecontent) / 1024 / 1024, 3)
-            print(f"Size: {filesize} MB")
+            filesize = round(len(response.read()) / 1024 / 1024, 3)
+            print(f"Size: {filesize}")
             data["downloadSize"] = filesize
 
         resultcontent = json.dumps(modlist, indent='\t', separators=(',', ' : ')) + "\n"
 
         if filecontent != resultcontent:
-            with open(filename, "w") as file:
-                file.write(resultcontent)
+            open(filename, "w").write(resultcontent)
 
 sys.exit(os.EX_OK)

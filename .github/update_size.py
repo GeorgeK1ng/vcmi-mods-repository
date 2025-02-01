866ff9679d5f62bc8d888266ecbcb49147d302b4
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2025-02-01 21:51:40 +0100

    Update update_size.py

--- .github/update_size.py
+++ .github/update_size.py
@@ -1,31 +1,53 @@
-import json
+import jstyleson
 import glob
 import os
 import sys
 import urllib.request
+from io import StringIO
 
 from ignore_json import ignore
 
 for filename in glob.glob(os.path.join('.', '*.json')):
     if filename not in ignore:
         print(f"Opening: {filename}")
-        filecontent = open(filename, "r").read()
-        modlist = json.loads(filecontent)
+
+        with open(filename, "r") as file:
+            filecontent = file.read()
+
+        try:
+            jstyleson.load(StringIO(filecontent))
+            modlist = jstyleson.loads(filecontent)
+        except Exception as err:
+            print(f"❌ Error reading JSON file {filename}: {err}")
+            sys.exit(os.EX_SOFTWARE)
+
         for mod, data in modlist.items():
             url = data["download"].replace(" ", "%20")
             print(f"Download {mod}: {url}")
+
             try:
                 response = urllib.request.urlopen(url)
-            except:
-                print("Error: download failed!")
+                print(f"✅ Download successful")
+            except Exception as err:
+                print(f"❌ Download failed: {err}")
                 sys.exit(os.EX_SOFTWARE)
-            filesize = round(len(response.read()) / 1024 / 1024, 3)
+
+            try:
+                filecontent = response.read().decode("utf-8")
+                jstyleson.load(StringIO(filecontent))
+                print(f"✅ JSON valid")
+            except Exception as err:
+                print(f"❌ JSON invalid: {err}")
+                sys.exit(os.EX_SOFTWARE)
+
+            filesize = round(len(filecontent.encode('utf-8')) / 1024 / 1024, 3)
             print(f"Size: {filesize}")
             data["downloadSize"] = filesize
 
         resultcontent = json.dumps(modlist, indent='\t', separators=(',', ' : ')) + "\n"
 
         if filecontent != resultcontent:
-            open(filename, "w").write(resultcontent)
+            with open(filename, "w") as file:
+                file.write(resultcontent)
 
 sys.exit(os.EX_OK)
